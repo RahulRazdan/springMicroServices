c102acce804c02720b3201dc63ebfb878596b9e8
RahulRazdan <razdanrahul0@gmail.com>
2018-07-04 09:08:36 +0530

    Swagger documentation added.

--- restful-web-services/src/main/java/com/spring/rest/mydemo/webservices/restfulwebservices/models/Users.java
+++ restful-web-services/src/main/java/com/spring/rest/mydemo/webservices/restfulwebservices/models/Users.java
@@ -5,14 +5,20 @@ import java.util.Date;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+@ApiModel(description="Contains information about all existing users")
 public class Users {
 
 	private int id;
 	
 	@Size(min=2,message="Name should be of at least length 2")
+	@ApiModelProperty(notes="At least of length 2")
 	private String name;
 	
 	@Past
+	@ApiModelProperty(notes="DOB should be in past")
 	private Date dob;
 
 	public int getId() {
